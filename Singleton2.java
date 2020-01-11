d877176cffa0930a2695be2e3a289ab6270c32be
pentilun <pentilun@163.com>
2020-01-11 23:22:44 +0800

    单例模式 懒汉式

---
+++ Singleton2.java
@@ -0,0 +1,13 @@
+public class Singleton2{
+    private static Singleton2 singleton2;
+
+    private Singleton2(){
+    }
+
+    public static synchronized Singleton2 getInstance(){
+        if(singleton2 == null){
+            singleton2 = new Singleton2();
+        }
+        return singleton2;
+    }
+}
